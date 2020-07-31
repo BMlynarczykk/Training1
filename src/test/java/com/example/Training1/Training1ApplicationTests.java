25f91c030ca4713dea7bebe177b43f04bb4ff2b6
BMlynarczykk <bartlomiej.mlynarczyk92@gmail.com>
2020-07-31 14:00:33 +0200

    Initial commit

---
+++ src/test/java/com/example/Training1/Training1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Training1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Training1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
